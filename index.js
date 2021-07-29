74ca5df33f3a6fb11611a798160ddb4d5dc8b7b0
keithkirui91 <keith.kirui33@gmail.com>
2021-07-29 14:50:56 +0300

    update files
    add return value of the  order price

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function denyDelivery(){
     alert("Pizza is ready for pickup at our Location");
 }
 
+
+
+
+
 function totalPrice(){
 
 var size = document.getElementsByName("size");
@@ -30,73 +34,60 @@ var deliveryfee = document.getElementsByName("deliveryDetails");
 
 
 if(size[0].checked){
-    var sizeprice = size[0].value;
-    
+    var sizeprice = parseInt(size[0].value);
+
 }
 else if(size[1].checked){
-    var sizeprice = size[1].value;
+    var sizeprice = parseInt(size[1].value);
 }
 else if(size[2].checked){
-    var sizeprice = size[2].value;
+    var sizeprice = parseInt(size[2].value);
 }
 
 
 if(crust[0].checked){
-    var crustprice = crust[0].value;
+    var crustprice = parseInt(crust[0].value);
 }
 else if(crust[1].checked){
-    var crustprice = crust[1].value;
+    var crustprice = parseInt(crust[1].value);
 }
 else if(crust[2].checked){
-    var crustprice = crust[2].value;
+    var crustprice = parseInt(crust[2].value);
 }
 else if(crust[3].checked){
-    var crustprice = crust[3].value;
+    var crustprice = parseInt(crust[3].value);
 }
 
 
 
 if(topping[0].checked){
-    var toppingprice = topping[0].value;
+    var toppingprice = parseInt(topping[0].value);
 }
 else if(topping[1].checked){
-    var toppingprice = topping[1].value;
+    var toppingprice = parseInt(topping[1].value);
 }
 else if(topping[2].checked){
-    var toppingprice = topping[2].value;
+    var toppingprice = parseInt(topping[2].value);
 }
 else if(topping[3].checked){
-    var toppingprice = topping[3].value;
+    var toppingprice = parseInt(topping[3].value);
 }
 if(deliveryDetails[0].checked){
-    var deliveryfee = deliveryDetails[0].value;
+    var deliveryfee = parseInt(deliveryDetails[0].value);
 }
 else if(deliveryDetails[1].checked){
-    var deliveryfee = deliveryDetails[1].value;
+    var deliveryfee = parseInt(deliveryDetails[1].value);
 }
 
+    var numberOfPizzas = parseInt(document.getElementById('numberOfPizzas').value);
+    var x = (crustprice + toppingprice + sizeprice + deliveryfee) * numberOfPizzas;
 
-    var x = crustprice + toppingprice + sizeprice + deliveryfee;
+    const totalCost = document.getElementById('total-Cost');
+    totalCost.innerHTML = ("its"+ x) ;
 
-    //const totalCost = document.getElementById('total-Cost');
-    alert("its"+ x) ;
-
-
-}
-
-function acceptDelivery(){
-    var location = prompt("Enter Your Location");
-    while(location === ''){
-        location = prompt("note::Location is required");
-    }
     
 
 
-    
-    alert("Your Delivery cost is 100sh. Pizza will be delivered to your location")
-}
-function denyDelivery(){
-    alert("Pizza is ready for pickup at our Location");
 }
 
 
